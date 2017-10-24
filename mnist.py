cd0c8e0f8796ff4eb00dcf05813b2afdeb0b2231
srijith123 <97srijithsasikumar@gmail.com>
2017-10-24 21:25:08 +0530

    Digital Recognisition

---
+++ mnist.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+
+data = pd.read_csv("train.csv")
+
+#importing the data from train.csv as matrix
+data = pd.read_csv("train.csv").as_matrix()
+print("Matrix Data\n",data)
+
+clf=DecisionTreeClassifier()
+
+#splitting with 50-50 
+
+X_train=data[0:21000, 1:]
+Y_train=data[0:21000, 0]
+
+X_test=data[21000:, 1:]
+Y_test=data[21000:, 0]
+clf.fit(X_train,Y_train)
+disp= X_test[8]
+disp.shape = (28,28)
+plt.imshow(255 - disp,cmap="gray")
+plt.show()
+
+p=clf.predict([X_test[8]])
+print("Prediction = ",p)
+
+
